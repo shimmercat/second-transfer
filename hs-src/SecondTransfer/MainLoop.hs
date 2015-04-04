c947b6b7c420f5f0f6f3b422924835344006d095
alcidesv <alcidesv@zunzun.se>
2015-04-04 06:05:33 +0200

    Improved docs

--- hs-src/SecondTransfer/MainLoop.hs
+++ hs-src/SecondTransfer/MainLoop.hs
@@ -1,5 +1,10 @@
 module SecondTransfer.MainLoop (
-	-- * Callback types
+	-- * Callbacks
+	--
+	-- | These callback make possible to separate the parts in layers. 
+	--   The `Attendant` is a function that can push and pull bytes to/from
+	--   a transport (for example, a socket), but it is not concerned on how
+	--   those bytes are pushed or pulled. 
 	Attendant
 	,PullAction
 	,PushAction
--- hs-src/SecondTransfer/MainLoop/PushPullType.hs
+++ hs-src/SecondTransfer/MainLoop/PushPullType.hs
@@ -18,8 +18,10 @@ import qualified Data.ByteString.Lazy         as LB
 
 -- | Callback type to push data to a channel. Part of this 
 --   interface is the abstract exception type IOProblem. Throw an 
---   instance of it to notify the session that the connection has 
---   been broken. 
+--   instance of it from here to notify the session that the connection has 
+--   been broken. There is no way to signal "normal termination", since 
+--   HTTP/2's normal termination can be observed at a higher level when a 
+--   GO_AWAY frame is seen. 
 type PushAction  = LB.ByteString -> IO ()
 
 -- | Callback type to pull data from a channel. The same
@@ -27,7 +29,7 @@ type PushAction  = LB.ByteString -> IO ()
 --   there. 
 type PullAction  = IO  B.ByteString
 
--- | Callback that the Session calls to realease resources 
+-- | Callback that the session calls to realease resources 
 --   associated with the channels. Take into account that your
 --   callback should be able to deal with non-clean shutdowns
 --   also, for example, if the connection to the remote peer
@@ -43,6 +45,10 @@ type CloseAction = IO ()
 --   push, pull and close callbacks. In this library we supply callbacks
 --   for TLS sockets, so that you don't need to go through the drudgery 
 --   of managing those yourself.
+--
+--   Attendants encapsulate all the session book-keeping functionality,
+--   which for HTTP/2 is quite complicated. You use the function `http2Attendant`
+--   to create one of these from a `CoherentWorker`.
 type Attendant = PushAction -> PullAction -> CloseAction -> IO () 
 
 
@@ -58,8 +64,8 @@ instance  Show IOProblem where
 instance Exception IOProblem 
 
 -- | A concrete case of the above exception. Throw one of this
---   if you don't want to implement your own type. Capture one 
---   `IOProblem` otherwise. 
+--   if you don't want to implement your own type. Use 
+--   `IOProblem` in catch signatures.
 data GenericIOProblem = GenericIOProblem
 	deriving (Show, Typeable)
 
