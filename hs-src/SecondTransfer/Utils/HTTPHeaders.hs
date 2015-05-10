babb883647f7ae44a012cb6a10f597aeab9a512f
alcidesv <alcidesv@zunzun.se>
2015-05-10 21:45:09 +0200

    Removed a few bugs and added a function

--- hs-src/SecondTransfer/Utils/HTTPHeaders.hs
+++ hs-src/SecondTransfer/Utils/HTTPHeaders.hs
@@ -1,10 +1,13 @@
 module SecondTransfer.Utils.HTTPHeaders (
     lowercaseHeaders
-    -- ,headersAreValidHTTP2
+    ,headersAreValidHTTP2
     ) where 
 
-import Data.Text.Encoding                (encodeUtf8, decodeUtf8)
-import Data.Text                         (toLower)
+import           Data.Char          (isUpper)
+import           Data.Text          (toLower)
+import qualified Data.Text          as T
+import           Data.Text.Encoding (decodeUtf8, encodeUtf8)
+
 
 import SecondTransfer.MainLoop.CoherentWorker      (Headers)
 
@@ -12,13 +15,21 @@ import SecondTransfer.MainLoop.CoherentWorker      (Headers)
 -- with Strings and is not very friendly to custom headers. 
 -- This is a very basic, lightweight normalizer.
 
--- ^ HTTP headers are case-insensitive, so we can use lowercase versions 
+-- | HTTP headers are case-insensitive, so we can use lowercase versions 
 -- everywhere
 lowercaseHeaders :: Headers -> Headers
 lowercaseHeaders = map (\(h,v) -> (low h, v))
   where 
     low = encodeUtf8 . toLower . decodeUtf8
 
--- ^ Checks that headers are lowercase
+
+-- | Checks that headers are lowercase
 headersAreValidHTTP2 :: Headers -> Bool 
-headersAreValidHTTP2 = error "Not implemented"
+headersAreValidHTTP2 headers = 
+  let 
+    isOk a_header = not . T.any isUpper . decodeUtf8 . fst $ a_header
+  in 
+    foldl
+        (\ prev e -> (flip (&&)) (isOk  e) $! prev)
+        True
+        headers
