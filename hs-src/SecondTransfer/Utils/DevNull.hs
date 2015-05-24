6af51102e6d605ddf5578132451c599d345fe3b4
alcidesv <alcidesv@zunzun.se>
2015-05-24 17:07:44 +0200

    - Make it non-blocking

--- hs-src/SecondTransfer/Utils/DevNull.hs
+++ hs-src/SecondTransfer/Utils/DevNull.hs
@@ -3,15 +3,22 @@ module SecondTransfer.Utils.DevNull(
     dropIncomingData
     ) where 
 
-import SecondTransfer.MainLoop.CoherentWorker 
+
+import Control.Concurrent (forkIO)
 import Data.Conduit 
 
+import SecondTransfer.MainLoop.CoherentWorker 
+
+-- TODO: Handling unnecessary data should be done in some other, less 
+-- harmfull way... need to think about that.
 
 -- | If you are not processing the potential POST input in a request,
 -- use this consumer to drop the data to oblivion. Otherwise it will 
--- remain in an internal queue until until the client closes the 
+-- remain in an internal queue until the client closes the 
 -- stream, and if the client doesn't want to do so....
 dropIncomingData :: Maybe InputDataStream -> IO ()
 dropIncomingData Nothing = return ()
-dropIncomingData (Just data_source) = 
-    data_source $$ (awaitForever (\ x -> return () ) )
+dropIncomingData (Just data_source) = do
+    forkIO $ 
+        data_source $$ (awaitForever (\ _ -> return () ) )
+    return ()
