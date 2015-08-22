ff2eebb4b7fb9dc18d064f3dfa744dadd93e62f7
Alcides Viamontes E <alcidesv@zunzun.se>
2015-08-22 19:29:10 +0200

    Very basic running of the example program, commented profiling variants

--- examples/attempt_bust/profile.sh
+++ examples/attempt_bust/profile.sh
@@ -3,14 +3,14 @@
 # Need to change dirs to get the test certificates
 pushd ../../
 #attempt-bust +RTS -hy -xt -i0.1 -RTS & 
-#attempt-bust +RTS -S -RTS & 
-valgrind --tool=massif attempt-bust & 
+attempt-bust +RTS -S -RTS & 
+#valgrind --tool=massif attempt-bust & 
 ppid="$!"
 echo "Pid: $ppid"
 
-for i in {1..500}
+for i in {1..10000}
 do
-    curl -s -X GET --http2 -k https://www.httpdos.com:9043/ # > /dev/null
+    curl -s -X GET --http2 -k https://www.httpdos.com:9043/ > /dev/null
     #sleep 0.1
     #echo "."
 done
