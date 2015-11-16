3cba44ac2747032e2d054ff655c6767c2446b5a8
Alcides Viamontes E <alcidesv@zunzun.se>
2015-11-16 06:35:54 +0100

    - Tests need to be fixed!

--- tests/tests-hs-src/Tests/HTTP1Parse.hs
+++ tests/tests-hs-src/Tests/HTTP1Parse.hs
@@ -26,14 +26,14 @@ import qualified SecondTransfer.Utils.HTTPHeaders    as He
 testParse :: Test
 testParse = TestCase $ do
     let
-        headers_text                   = "GET /helo.html HTTP/1.1\r\nHost: www.auther.com \r\n\r\n"
-        headers_text2                   = "POST /helo.html HTTP/1.1\r\nHost: www.auther.com \r\nContent-Length: 1\r\n\r\np"
-        a0                             = newIncrementalHttp1Parser
-        isDone (OnlyHeaders_H1PC _ _)  = True
-        isDone _                       = False
-        a1                             = addBytes a0 headers_text
-        (OnlyHeaders_H1PC h0 leftovers)= a1
-        (HeadersAndBody_H1PC h1 cond0 l2) = addBytes a0 headers_text2
+        headers_text                       = "GET /helo.html HTTP/1.1\r\nHost: www.auther.com \r\n\r\n"
+        headers_text2                      = "POST /helo.html HTTP/1.1\r\nHost: www.auther.com \r\nContent-Length: 1\r\n\r\np"
+        a0                                 = newIncrementalHttp1Parser
+        isDone (OnlyHeaders_H1PC _ _)      = True
+        isDone _                           = False
+        a1                                 = addBytes a0 headers_text
+        (OnlyHeaders_H1PC h0 leftovers)    = a1
+        (HeadersAndBody_H1PC h1 cond0 l2)  = addBytes a0 headers_text2
         waitForBodyOk (HeadersAndBody_H1PC _ _ _)   = True
         waitForBodyOk _ = False
     assertBool "testParse.IsDone" (isDone a1)
